22b8a2db89faa92a39bd3413f05ab1b638064615
debojit <saha05debojit@gmail.com>
2022-10-16 14:04:50 +0530

    Added Login & Signup UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import { Button } from '@chakra-ui/react'
+import { Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme='blue' my={3}>Button</Button>
+      <Route exact path= '/' component={Homepage} />
+      <Route exact path= '/chats' component={ChatPage} />
     </div>
   );
 }
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Container,
+  Box,
+  Heading,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import Signup from "../components/Authentication/Signup";
+import Login from "../components/Authentication/Login";
+
+const Homepage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Heading fontFamily="Work sans"> TALKATIVE </Heading>
+      </Box>
+
+      <Box bg="white" p={4} w="100%" borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="blue">
+          <TabList mb='0.2em'>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
